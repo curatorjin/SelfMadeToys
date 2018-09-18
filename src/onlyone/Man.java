8cd18a7a645228242d338e27e11b32cc5336aaa3
阿金 <disciple_sneaker@163.com>
2018-09-18 17:14:52 +0800

    Create Man.java

---
+++ src/onlyone/Man.java
@@ -0,0 +1,54 @@
+package onlyone;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+
+public class Man extends Player{
+	
+	@Override
+	public Card draw(Player player, BufferedReader br) {
+		while(true) {
+			System.out.println("请选择要抽取的种类：");
+			System.out.println("1.现场\t2.犯人\t3.动机\t4.凶器");
+			String input = "";
+			try {
+				input = br.readLine();
+				switch (input) {
+				case "1":
+					input = "现场";
+					break;
+				case "2":
+					input = "犯人";
+					break;
+				case "3":
+					input = "动机";
+					break;
+				case "4":
+					input = "凶器";
+					break;
+				default:
+					break;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if ("".equals(input)) {
+				System.out.println("请输入正确的值！");
+				continue;
+			}
+			List<Card> listForDraw = player.getHandDeck().getKind(input);
+			if (listForDraw.size() == 0) {
+				System.out.println("对方已经没有这种牌了……");
+				continue;
+			}else {
+				Random random = new Random();
+				int i = random.nextInt(listForDraw.size());
+				Card drewOne = listForDraw.get(i);
+				return drewOne;
+			}
+		}
+	}
+
+}
