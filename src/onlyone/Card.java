2b789c43ba240c07d555f9683e06cb9e6f0aa66a
阿金 <disciple_sneaker@163.com>
2018-09-18 17:12:57 +0800

    Create Card.java

---
+++ src/onlyone/Card.java
@@ -0,0 +1,66 @@
+package onlyone;
+
+public class Card {
+	private String kind;
+	private String content;
+	
+	public Card() {}
+	
+	public Card(String kind, String content) {
+		super();
+		this.kind = kind;
+		this.content = content;
+	}
+
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((kind == null) ? 0 : kind.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Card other = (Card) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (kind == null) {
+			if (other.kind != null)
+				return false;
+		} else if (!kind.equals(other.kind))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return kind + "：" + content;
+	}
+	
+	
+	
+}
