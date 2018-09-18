f2d5abf1f2f9723d266ced7c93a8690754baa67d
阿金 <disciple_sneaker@163.com>
2018-09-18 17:15:11 +0800

    Create Player.java

---
+++ src/onlyone/Player.java
@@ -0,0 +1,46 @@
+package onlyone;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Player {
+	
+	private Deck handStuff = new Deck();
+	
+	public Deck getHandDeck() {
+		return handStuff;
+	}
+	
+	public List<Card> getHandStuff() {
+		return handStuff.getCards();
+	}
+
+	public List<Card> getKind(String kind){
+		List<Card> list = new ArrayList<>();
+		for (Card card:handStuff.getCards()) {
+			if (card.getKind().equals(kind)) {
+				list.add(card);
+			}
+		}
+		return list;
+	}
+	
+	public void modify() {
+		List<Card> list = new ArrayList<>(getHandStuff());
+		List<Card> hand = new ArrayList<>();
+		for (Card card : list) {
+			if (hand.contains(card)) {
+				Deck.putToDeskDeck(card);
+				Deck.putToDeskDeck(card);
+				hand.remove(card);
+			}else {
+				hand.add(card);
+			}
+		}
+		getHandDeck().setCards(hand);
+	}
+	
+	abstract Card draw(Player player, BufferedReader br);
+}
