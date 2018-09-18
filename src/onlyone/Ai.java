c456b1ac77dfe89cd0b4ec2ba1cad6014c2e4981
阿金 <disciple_sneaker@163.com>
2018-09-18 17:11:48 +0800

    Create Ai.java

---
+++ src/onlyone/Ai.java
@@ -0,0 +1,21 @@
+package onlyone;
+
+import java.io.BufferedReader;
+import java.util.List;
+import java.util.Random;
+
+public class Ai extends Player{
+	
+	@Override
+	public Card draw(Player player, BufferedReader br) {
+		List<Card> stuff = player.getHandStuff();
+		if (stuff.size() == 0) {
+			return null;
+		}else {
+			Random random = new Random();
+			int i = random.nextInt(stuff.size());
+			return stuff.remove(i);
+		}
+	}
+
+}
